17277372efac5d0de68ae0c9e2edeb23f3b629bf
dhilip143 <kdhilip146@gmail.com>
2025-11-14 18:07:25 +0530

    wantpulp

--- src/Pages/sales/SalesLogin.jsx
+++ src/Pages/sales/SalesLogin.jsx
@@ -16,7 +16,7 @@ function SalesLogin() {
 
     try {
       const response = await axios.post(
-        "http://127.0.0.1:8000/blog/login-sales-register/",
+        "http://127.0.0.1:8000/blog/login-admin-sales/",
         {
           email: email,
           password: password,
